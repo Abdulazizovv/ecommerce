977478309ee74dc86b617eb086c8bbca09d7b2d4
Ulugbek Abdulazizov <aqllibola567@gmail.com>
2025-08-01 07:54:03 +0500

    changed docs url and redirect to docs

--- core/urls.py
+++ core/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework import permissions
@@ -21,26 +22,44 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Ecommerce API",
-      default_version='v1',
-      description="Ecommerce loyihasi uchun Swagger hujjati",
-      terms_of_service="http://localhost/terms/",
-      contact=openapi.Contact(email="aqllibola@gmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Ecommerce API",
+        default_version="v1",
+        description="Ecommerce loyihasi uchun Swagger hujjati",
+        terms_of_service="http://localhost/terms/",
+        contact=openapi.Contact(email="aqllibola@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
 
+def redirect_to_docs(request):
+    """
+    Redirect root URL to Swagger documentation.
+    """
+    from django.shortcuts import redirect
+
+    return redirect("schema-swagger-ui")
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
     # API
-    path('api/', include('apps.api.urls')),
-    
+    path("api/", include("apps.api.urls")),
     # Swagger
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # Redirect root URL to Swagger docs
+    path("", redirect_to_docs, name="redirect-to-docs"),
 ]
